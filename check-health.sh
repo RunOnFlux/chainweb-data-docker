c311872204579c57cc7ed8e59aacf35c6f1a85b7
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-16 16:54:53 +0200

    clean

--- check-health.sh
+++ check-health.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
 check=$(supervisorctl status | egrep  'chainweb-data|postgres' | grep RUNNING | wc -l)
-
 if [[ "$check" == 2 ]]; then 
-
-
   if [[ -f "/tmp/backfill" ]]; then
     echo "chainweb-data: running, postgres: running, backfill: complited"
   else
     echo "chainweb-data: running, postgres: running, backfill: not complited"
   fi
-
 else 
-exit 1
+  exit 1
 fi
