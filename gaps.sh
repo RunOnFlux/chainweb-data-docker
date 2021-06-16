d271f8dd38e420dda7099e367a2fb835bfc5e10b
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-16 16:50:49 +0200

    added logfile for gaps

--- gaps.sh
+++ gaps.sh
@@ -6,6 +6,7 @@ if [[ ! -f /tmp/backfill ]]; then
   echo -n "Backfill not complited...Gaps skipped!"
   exit
 fi
-
+date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+echo -n "Gaps tiggered at $date_timestamp" >> /tmp/gaps.log
 chainweb-data gaps --service-host=172.15.0.1 --p2p-host=172.15.0.1 --service-port=30005 --p2p-port=30004 --dbuser=postgres --dbpass=postgres --dbname=postgres
 
