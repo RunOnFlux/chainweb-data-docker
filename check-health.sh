1cc7f3445c4029d6a40cf3af20580d59686ab5b8
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-17 22:14:59 +0200

    added backfill progress

--- check-health.sh
+++ check-health.sh
@@ -5,7 +5,8 @@ if [[ "$check" == 2 ]]; then
   if [[ -f "/tmp/backfill" ]]; then
     echo "chainweb-data: running, postgres: running, backfill: complited"
   else
-    echo "chainweb-data: running, postgres: running, backfill: not complited"
+    progress=$(cat $(ls /var/log/supervisor | grep chainweb-backfill-stdout | awk {'print "/var/log/supervisor/"$1'} ) | tail -n1 | egrep -o -E '[0-9]+\.[0-9]+.')
+    echo "chainweb-data: running, postgres: running, backfill: running ($progress)"
   fi
 else 
   exit 1
