a930e13bb8002ffb7a78389f5611fd45d29ce695
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-18 14:15:36 +0200

    added gaps history logs

--- gaps.sh
+++ gaps.sh
@@ -7,7 +7,7 @@ if [[ ! -f /tmp/backfill ]]; then
 fi
 
 date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-echo -n "Gaps tiggered at $date_timestamp" >> /tmp/gaps_time.log
+echo -n "Gaps was started at $date_timestamp" >> /tmp/gaps_history.log
 /usr/local/bin/chainweb-data gaps --service-host=172.15.0.1 --p2p-host=172.15.0.1 --service-port=30005 --p2p-port=30004 --dbuser=postgres --dbpass=postgres --dbname=postgres
-
-
+date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+echo -n "Gaps was ended at $date_timestamp" >> /tmp/gaps_history.log
