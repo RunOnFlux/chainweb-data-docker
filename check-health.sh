3fb4c3ef3246304d4a73caf64fc50130c8ea74eb
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-19 12:01:45 +0200

    added remaining time for backfill

--- check-health.sh
+++ check-health.sh
@@ -4,24 +4,23 @@ check=$(supervisorctl status | egrep  'chainweb-data|postgres' | grep RUNNING | 
 
 if [[ "$check" == 2 ]]; then
 
-check_api_listening=$(sudo lsof -i -P -n | grep -o 8888)
-
+  check_api_listening=$(sudo lsof -i -P -n | grep -o 8888)
   if [[ "$check_api_listening" != "" ]]; then
     status="ONLINE"
   else
     status="OFFLINE"
   fi
 
-  if [[ -f "/tmp/backfill" ]]; then
+ if [[ -f "/tmp/backfill" ]]; then
     echo "chainweb-data: running ($status), postgres: running, backfill: complited"
   else
-    progress=$(cat $(ls /var/log/supervisor | grep chainweb-backfill-stdout | awk {'print "/var/log/supervisor/"$1'} ) | tail -n1 | egrep -o -E '[0-9]+\.[0-9]+.')
+    progress=$(cat $(ls /var/log/supervisor | grep chainweb-backfill-stdout | awk {'print "/var/log/supervisor/"$1'} ) | tail -n1 | egrep -o -E '.[0-9]+\.[0-9]+.{5}[0-9]+.{1,20}.remaining')
     if [[ "$progress" == "" ]]; then
-     progress='awaiting...'
+      progress='awaiting...'
     fi
-    echo "chainweb-data: running ($status), postgres: running, backfill: running ($progress)"
+    echo "chainweb-data: running ($status), postgres: running, backfill: running $progress"
   fi
-
+  
 else
   exit 1
 fi
