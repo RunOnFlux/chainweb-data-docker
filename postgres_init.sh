8e1beaa8a6bf8b1cf3512203a63d12a91a74b452
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-19 09:30:06 +0200

    removed duplicate

--- postgres_init.sh
+++ postgres_init.sh
@@ -3,8 +3,6 @@
 
 if [[ ! -f /var/lib/postgresql/data/postgresql.conf ]]; then
  echo -n "Postgres initialization..."
- chown -R postgres:postgres /var/lib/postgresql/data
- chmod 700 -R /var/lib/postgresql/data/*
  cp -rf /etc/postgresql/13/main/* /var/lib/postgresql/data
  cp -rf /var/lib/postgresql/13/main/* /var/lib/postgresql/data
  chown -R postgres:postgres /var/lib/postgresql/data
