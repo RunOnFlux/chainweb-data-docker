47fcdb500961af7194c9f6e1cfaad27cd690433c
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-21 07:42:54 +0200

    fix echo

--- backfill.sh
+++ backfill.sh
@@ -4,13 +4,13 @@ check=$(curl -SsL -k -m 15 https://172.15.0.1:30004/chainweb/0.0/mainnet01/cut |
 if [[ "$check" == "" ]]; then
   until [ $check != "" ] ; do
     check=$(curl -SsL -k -m 15 https://172.15.0.1:30004/chainweb/0.0/mainnet01/cut | jq .height)
-    echo -n "Awaiting for KDA node..."
+    echo -e "Awaiting for KDA node..."
     sleep 300
   done
 fi
 
 if [[ -f /tmp/backfill ]]; then
-  echo -n "Backfill already done! skipped..."
+  echo -e "Backfill already done! skipped..."
   exit
 fi
 
@@ -22,7 +22,7 @@ until [ $x == 1 ] ; do
     chainweb-data backfill --service-host=172.15.0.1 --p2p-host=172.15.0.1 --service-port=30005 --p2p-port=30004 --dbuser=postgres --dbpass=postgres --dbname=postgres
     sleep 5
     x=1
-    echo "B1ackfill complited!" >> /tmp/backfill
+    echo -e "B1ackfill complited!" >> /tmp/backfill
     (crontab -l -u "$USER" 2>/dev/null; echo "30 22 * * *  /bin/bash /gaps.sh > /tmp/gaps_output.log 2>&1") | crontab -
   fi
 done
