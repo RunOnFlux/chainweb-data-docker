dfa28ca488957fc4d4c55c7c262636b17c7d2cc0
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-21 07:44:26 +0200

    fix echo

--- postgres_init.sh
+++ postgres_init.sh
@@ -2,7 +2,7 @@
 # postgres init script
 
 if [[ ! -f /var/lib/postgresql/data/postgresql.conf ]]; then
- echo -n "Postgres initialization..."
+ echo -e "Postgres initialization..."
  cp -rf /etc/postgresql/13/main/* /var/lib/postgresql/data
  cp -rf /var/lib/postgresql/13/main/* /var/lib/postgresql/data
  chown -R postgres:postgres /var/lib/postgresql/data
@@ -10,7 +10,7 @@ if [[ ! -f /var/lib/postgresql/data/postgresql.conf ]]; then
  chmod 700 -R /var/lib/postgresql/data
  sleep 10
 else
- echo -n "Postgres already initialized!"
+ echo -e "Postgres already initialized!"
  chown -R postgres:postgres /var/lib/postgresql/data
  chmod 700 -R /var/lib/postgresql/data/*
  chmod 700 -R /var/lib/postgresql/data
