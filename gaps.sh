2aadca97dff2e5bba7134cd7d8c3a7f4501dc364
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-24 16:04:39 +0200

    restart chainwebdata after gaps

--- gaps.sh
+++ gaps.sh
@@ -11,3 +11,5 @@ echo -e "Gaps was started at $date_timestamp" >> /tmp/gaps_history.log
 /usr/local/bin/chainweb-data gaps --service-host=172.15.0.1 --p2p-host=172.15.0.1 --service-port=30005 --p2p-port=30004 --dbuser=postgres --dbpass=postgres --dbname=postgres
 date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 echo -e "Gaps was ended at $date_timestamp" >> /tmp/gaps_history.log
+echo -e "Restarting chainweb-data..."
+kill -9 $(ps aux | grep 'chainweb-data server --port 8888' | awk '{ print $2 }' | head -n1)
