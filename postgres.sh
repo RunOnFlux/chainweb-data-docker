03adb3bee7e3228c28bff569d51786d479f121ca
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-25 12:02:35 +0200

    postgres run script

---
+++ postgres.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# postgres luncher
+
+x=0
+until [ $x == 1 ] ; do
+
+echo -e "Waiting for postgres marker..."
+sleep 180
+
+  if [[ -f /tmp/postgres_start ]]; then
+  echo -e "Starting postgres..."
+    x=1
+  fi
+
+
+done
+
+/usr/lib/postgresql/13/bin/postgres -D /var/lib/postgresql/data -c config_file=/var/lib/postgresql/data/postgresql.conf
