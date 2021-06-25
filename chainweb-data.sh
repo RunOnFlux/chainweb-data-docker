10b5f4e8e1cbac17ee16e05b2263f2e873cf1d81
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-06-25 12:13:23 +0200

    added backfill marker

--- chainweb-data.sh
+++ chainweb-data.sh
@@ -12,9 +12,7 @@ function node_await() {
  fi
 }
 
-
 x=0
-
 until [ $x == 1 ] ; do
 
 echo -e "Waiting for chainwebdata_build marker..."
@@ -32,6 +30,7 @@ done
 
  if [[ -f /usr/local/bin/chainweb-data ]]; then
    node_await
+   echo "Backfill start!" >> /tmp/chainwebdata_backfill
    chainweb-data server --port 8888 --service-host=172.15.0.1 --p2p-host=172.15.0.1 --service-port=30005 --p2p-port=30004 --dbuser=postgres --dbpass=postgres --dbname=postgres
    exit
  fi
@@ -100,5 +99,6 @@ done
  #starting chainweb-data server
  if [[ -f /usr/local/bin/chainweb-data ]]; then
      node_await
+     echo "Backfill start!" >> /tmp/chainwebdata_backfill
      chainweb-data server --port 8888 --service-host=172.15.0.1 --p2p-host=172.15.0.1 --service-port=30005 --p2p-port=30004 --dbuser=postgres --dbpass=postgres --dbname=postgres 
  fi
