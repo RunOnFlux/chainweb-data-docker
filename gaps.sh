c538dfd2655d618c692f022a56f7d7b5e9a0e844
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-08-13 13:53:28 +0200

    Minor edits

--- gaps.sh
+++ gaps.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-# chainweb-data fill
 
 if [[ ! -f /tmp/backfill ]]; then
-
-  if [[ ! -f /tmp/bootstrap ]]; then
-    echo -e "Fill not complited...skipped!"
-    exit
-  fi
-  
+    echo -e "Fill not complited...skipped..."  
 fi
 
 date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
@@ -15,5 +9,3 @@ echo -e "Fill was started at $date_timestamp" >> /tmp/fill_history.log
 /usr/local/bin/chainweb-data fill --service-host=172.15.0.1 --p2p-host=172.15.0.1 --service-port=30005 --p2p-port=30004 --dbuser=postgres --dbpass=postgres --dbname=postgres
 date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 echo -e "Fill was ended at $date_timestamp" >> /tmp/fill_history.log
-#echo -e "Restarting chainweb-data..."
-#kill -9 $(ps aux | grep 'chainweb-data server --port 8888' | awk '{ print $2 }' | head -n1)
