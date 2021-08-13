ab3f50116deb6c3aa2b687588b8487606178f0ea
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-08-13 13:51:18 +0200

    Minor edits

--- postgres_init.sh
+++ postgres_init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# postgres init script
 
 if [[ ! -f /var/lib/postgresql/data/postgresql.conf ]]; then
  echo -e "Postgres initialization..."
@@ -10,14 +9,14 @@ if [[ ! -f /var/lib/postgresql/data/postgresql.conf ]]; then
  chmod 700 -R /var/lib/postgresql/data
  sleep 10
 else
- echo -e "Postgres already initialized!"
+ echo -e "Postgres already initialized..."
  chown -R postgres:postgres /var/lib/postgresql/data
  chmod 700 -R /var/lib/postgresql/data/*
  chmod 700 -R /var/lib/postgresql/data
  sleep 10
 fi
 
-echo "Postgres start!" >> /tmp/postgres_start
+echo "Postgres start..." >> /tmp/postgres_start
 sleep 180
 echo "Chainweb-data build!" >> /tmp/chainwebdata_build
 
