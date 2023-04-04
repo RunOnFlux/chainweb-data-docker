3a5edc0ff14011f82ca810ae90434e8c5b5469cc
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-04-04 16:00:44 +0200

    Update postgres_init.sh

--- postgres_init.sh
+++ postgres_init.sh
@@ -7,17 +7,11 @@ if [[ ! -f /var/lib/postgresql/data/postgresql.conf ]]; then
  chown -R postgres:postgres /var/lib/postgresql/data
  chmod 700 -R /var/lib/postgresql/data/*
  chmod 700 -R /var/lib/postgresql/data
- sleep 10
 else
  echo -e "Postgres already initialized..."
  chown -R postgres:postgres /var/lib/postgresql/data
  chmod 700 -R /var/lib/postgresql/data/*
  chmod 700 -R /var/lib/postgresql/data
- sleep 10
 fi
 
 echo "Postgres start..." >> /tmp/postgres_start
-sleep 180
-echo "Chainweb-data build!" >> /tmp/chainwebdata_build
-
-
