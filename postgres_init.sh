7454c6146f6905f1a3d08d5125e1b43e8f1adeff
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-04-05 13:02:18 +0200

    added PG_VERSION

--- postgres_init.sh
+++ postgres_init.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ ! -f /var/lib/postgresql/data/postgresql.conf ]]; then
  echo -e "Postgres initialization..."
- cp -rf /etc/postgresql/15/main/* /var/lib/postgresql/data
- cp -rf /var/lib/postgresql/15/main/* /var/lib/postgresql/data
+ cp -rf /etc/postgresql/${PG_VERSION}/main/* /var/lib/postgresql/data
+ cp -rf /var/lib/postgresql/${PG_VERSION}/main/* /var/lib/postgresql/data
  chown -R postgres:postgres /var/lib/postgresql/data
  chmod 700 -R /var/lib/postgresql/data/*
  chmod 700 -R /var/lib/postgresql/data
