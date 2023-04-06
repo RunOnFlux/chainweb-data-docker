97a31df2b565a7fb23a4a94c9946c90ddf0571c3
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-04-06 09:58:19 +0200

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 # postgres luncher
 
-x=0
-until [[ "$x" == 1 ]] ; do
-  echo -e "Waiting for postgres marker..."
-  if [[ -f /tmp/postgres_start ]]; then
-    echo -e "Starting postgres..."
-    sleep 10
-    x=1
-  else
-    sleep 15
-  fi
-done
-
+if [[ "$1" == "start" ]]; then
+  x=0
+  until [[ "$x" == 1 ]] ; do
+    echo -e "Waiting for postgres marker..."
+    if [[ -f /tmp/postgres_start ]]; then
+      echo -e "Starting postgres..."
+      sleep 10
+      x=1
+    else
+      sleep 15
+    fi
+  done
 /usr/lib/postgresql/${PG_VERSION}/bin/postgres -D /var/lib/postgresql/data -c config_file=/var/lib/postgresql/data/postgresql.conf
+fi
+
+if [[ "$1" == "stop" ]]; then
+  echo -e "Stopping postgres..."
+  supervisorctl stop postgres > /dev/null 2>&1
+  pg_ctlcluster ${PG_VERSION} main stop -m fast
+fi
