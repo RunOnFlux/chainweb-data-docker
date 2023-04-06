d8b48bea895a5f9b1d7500681f76900dd67a1c4b
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-04-06 11:04:19 +0200

    added backup and restore

--- postgres.sh
+++ postgres.sh
@@ -12,3 +12,25 @@ if [[ "$1" == "stop" ]]; then
   supervisorctl stop postgres_start
   pg_ctlcluster ${PG_VERSION} main stop -m fast
 fi
+
+if [[ "$1" == "backup" ]]; then
+  echo -e "Starting backup..."
+  mkdir -p  /var/lib/postgresql/data/backup
+  cd /var/lib/postgresql/data/backup
+  if [[ -f /var/lib/postgresql/data/backup/chainweb-data-backup.tar ]]; then
+    rm -rf /var/lib/postgresql/data/backup/chainweb-data-backup.tar
+  fi
+  pg_dump -U postgres -Fc postgres > chainweb-data-backup.tar
+  size=$(ls -lh chainweb-data-backup.tar | awk '{ print $5 }')
+  echo -e "Backup created, size: $size"
+fi
+
+if [[ "$1" == "restore" ]]; then
+  echo -e "Restoring backup..."
+  if [[ -f /var/lib/postgresql/data/backup/chainweb-data-backup.tar ]]; then
+    pg_restore -d postgres /var/lib/postgresql/data/backup/chainweb-data-backup.tar -c -U postgres
+    echo -e "Operation complited!"
+  else
+    echo -e "Backup archive not found!"
+  fi
+fi
