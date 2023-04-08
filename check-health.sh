0e0ef96f21bb4057fb75bf23a5e2306df65a01f6
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-04-08 13:04:58 +0200

    removed path duplicate

--- check-health.sh
+++ check-health.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 PATH_DATA="/var/lib/postgresql/data"
-LOG_PATH="/var/lib/postgresql/data/fill.log"
-
 check=$(supervisorctl status | egrep  'chainweb-data|postgres' | grep RUNNING | wc -l)
 if [[ "$check" == 2 ]]; then
   check_api_listening=$(lsof -i -P -n | grep -o 8888)
@@ -10,14 +8,14 @@ if [[ "$check" == 2 ]]; then
   else
     status="OFFLINE"
   fi
- if [[ -f /var/lib/postgresql/data/BACKFILL ]]; then
+ if [[ -f $PATH_DATA/BACKFILL ]]; then
      echo "chainweb-data: running ($status), postgres: running, fill: complited"
  else
-    progress=$(cat $LOG_PATH | egrep -o -E 'Progress: [0-9]+.{5}[0-9]+.*' | egrep -o '[0-9]+\.[0-9]+' | tail -n1)
+    progress=$(cat $PATH_DATA/fill.log | egrep -o -E 'Progress: [0-9]+.{5}[0-9]+.*' | egrep -o '[0-9]+\.[0-9]+' | tail -n1)
      if [[ "$progress" == "" ]]; then
        progress='awaiting...'
      else
-       freez_check=$(cat $LOG_PATH | egrep -o 'Progress:.*[0-9]+\.[0-9]+.*' | egrep -o '[0-9]+\.[0-9]+' | tail -n3 | awk '{sum += $1} END {print sum-(3*$1)}')
+       freez_check=$(cat $PATH_DATA/fill.log | egrep -o 'Progress:.*[0-9]+\.[0-9]+.*' | egrep -o '[0-9]+\.[0-9]+' | tail -n3 | awk '{sum += $1} END {print sum-(3*$1)}')
        if [[ "$freez_check" == 0 ]]; then
           echo "Postgres info: insert query hangs, fill not finished! PC restart required"
           exit 1
